b16d6712c4ea0731f03b773131d1fde47efefa78
ahmadali shafiee <mail@ahmadalli.net>
2021-01-10 19:37:47 +0330

    gluster replicated volume module
    Signed-off-by: ahmadali shafiee <mail@ahmadalli.net>

---
+++ plugins/modules/gluster_replicated_volume.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python
+
+# this file is derived from gluster's ansible collection's gluster_volume module
+#   https://github.com/gluster/gluster-ansible-collection/blob/bc0982a24e85f3829ee19cd62da64e82601870a6/plugins/modules/gluster_volume.py
+
+import platform
+from ansible.module_utils.basic import AnsibleModule
+
+from .helper import GlusterVolumeHelper
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            name=dict(type='str', required=True, aliases=['volume']),
+            state=dict(type='str', required=True, choices=[
+                       'absent', 'started', 'stopped', 'present']),
+            cluster=dict(type='list'),
+            host=dict(type='str'),
+            transport=dict(type='str', default='tcp', choices=[
+                           'tcp', 'rdma', 'tcp,rdma']),
+            bricks=dict(type='list', aliases=['brick']),
+            start_on_create=dict(type='bool', default=True),
+            options=dict(type='dict', default={}),
+            force=dict(type='bool', default=False),
+        ),
+    )
+
+    changed = False
+
+    action = module.params['state']
+    volume_name = module.params['name']
+    cluster = module.params['cluster']
+    brick_paths = module.params['bricks']
+    transport = module.params['transport']
+    hostname = module.params['host']
+    start_on_create = module.boolean(module.params['start_on_create'])
+    force = module.boolean(module.params['force'])
+
+    if not hostname:
+        hostname = platform.node()
+
+    # Clean up if last element is empty. Consider that yml can look like this:
+    #   cluster="{% for host in groups['glusterfs'] %}{{ hostvars[host]['private_ip'] }},{% endfor %}"
+    if cluster is not None and len(cluster) > 1 and cluster[-1] == '':
+        cluster = cluster[0:-1]
+
+    if cluster is None:
+        cluster = []
+
+    if brick_paths is None:
+        brick_paths = []
+
+    helper = GlusterVolumeHelper(module)
+
+    volumes = helper.get_volumes()
+
+    if action == 'absent':
+        if volume_name in volumes:
+            if volumes[volume_name]['status'].lower() != 'stopped':
+                helper.stop_volume(volume_name)
+            helper.delete_volume(volume_name)
+            changed = True
+
+    if action == 'present':
+        # create if it doesn't exist
+        if volume_name not in volumes:
+            helper.create_volume(volume_name, transport,
+                                 cluster, brick_paths, force)
+            volumes = helper.get_volumes()
+            changed = True
+
+        if volume_name in volumes:
+            if volumes[volume_name]['status'].lower() != 'started' and start_on_create:
+                helper.start_volume(volume_name)
+                changed = True
+
+            # switch bricks
+            new_bricks = []
+            removed_bricks = []
+            all_bricks = []
+            bricks_in_volume = volumes[volume_name]['bricks']
+
+            for node in cluster:
+                for brick_path in brick_paths:
+                    brick = '%s:%s' % (node, brick_path)
+                    all_bricks.append(brick)
+                    if brick not in bricks_in_volume:
+                        new_bricks.append(brick)
+
+            if not new_bricks and len(all_bricks) > 0 and len(all_bricks) < len(bricks_in_volume):
+                for brick in bricks_in_volume:
+                    if brick not in all_bricks:
+                        removed_bricks.append(brick)
+
+            if new_bricks:
+                helper.add_bricks(volume_name, new_bricks,
+                                  len(all_bricks), force)
+                changed = True
+
+            if removed_bricks:
+                helper.remove_bricks(
+                    volume_name, removed_bricks, len(all_bricks))
+                changed = True
+        else:
+            module.fail_json(msg='failed to create volume %s' % volume_name)
+
+    if action != 'absent' and volume_name not in volumes:
+        module.fail_json(msg='volume not found %s' % volume_name)
+
+    if action == 'started':
+        if volumes[volume_name]['status'].lower() != 'started':
+            helper.start_volume(volume_name)
+            changed = True
+
+    if action == 'stopped':
+        if volumes[volume_name]['status'].lower() != 'stopped':
+            helper.stop_volume(volume_name)
+            changed = True
+
+    facts = {}
+    facts['glusterfs'] = {'volumes': volumes}
+
+    module.exit_json(changed=changed, ansible_facts=facts)
+
+if __name__ == '__main__':
+    main()
---
+++ plugins/modules/helper.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+
+# this file is derived from gluster's ansible collection's gluster_volume module
+#   https://github.com/gluster/gluster-ansible-collection/blob/bc0982a24e85f3829ee19cd62da64e82601870a6/plugins/modules/gluster_volume.py
+
+
+import traceback
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils._text import to_native
+
+
+class GlusterVolumeHelper(object):
+    _module: AnsibleModule = None
+    _gluster_bin: str = ''
+
+    def __init__(self, module: AnsibleModule):
+        self._module = module
+        self._gluster_bin = self._module.get_bin_path('gluster', True)
+
+    def run_gluster(self, gargs: list, **kwargs):
+        args = [self._gluster_bin, '--mode=script']
+        args.extend(gargs)
+        try:
+            rc, out, err = module.run_command(args, **kwargs)
+            if rc != 0:
+                module.fail_json(msg='error running gluster (%s) command (rc=%d): %s' %
+                                 (' '.join(args), rc, out or err), exception=traceback.format_exc())
+        except Exception as e:
+            module.fail_json(msg='error running gluster (%s) command: %s' % (' '.join(args),
+                                                                             to_native(e)), exception=traceback.format_exc())
+        return out
+
+    def run_gluster_nofail(self, gargs: list, **kwargs):
+        args = [self._gluster_bin]
+        args.extend(gargs)
+        rc, out, err = module.run_command(args, **kwargs)
+        if rc != 0:
+            return None
+        return out
+
+    def get_volumes(self):
+        out = self.run_gluster(['volume', 'info'])
+
+        volumes = {}
+        volume = {}
+        for row in out.split('\n'):
+            if ': ' in row:
+                key, value = row.split(': ')
+                if key.lower() == 'volume name':
+                    volume['name'] = value
+                    volume['options'] = {}
+                    volume['quota'] = False
+                if key.lower() == 'volume id':
+                    volume['id'] = value
+                if key.lower() == 'status':
+                    volume['status'] = value
+                if key.lower() == 'transport-type':
+                    volume['transport'] = value
+                if value.lower().endswith(' (arbiter)'):
+                    if 'arbiters' not in volume:
+                        volume['arbiters'] = []
+                    value = value[:-10]
+                    volume['arbiters'].append(value)
+                elif key.lower() == 'number of bricks':
+                    volume['replicas'] = value[-1:]
+                if key.lower() != 'bricks' and key.lower()[:5] == 'brick':
+                    if 'bricks' not in volume:
+                        volume['bricks'] = []
+                    volume['bricks'].append(value)
+                # Volume options
+                if '.' in key:
+                    if 'options' not in volume:
+                        volume['options'] = {}
+                    volume['options'][key] = value
+                    if key == 'features.quota' and value == 'on':
+                        volume['quota'] = True
+            else:
+                if row.lower() != 'bricks:' and row.lower() != 'options reconfigured:':
+                    if len(volume) > 0:
+                        volumes[volume['name']] = volume
+                    volume = {}
+        return volumes
+
+    def start_volume(self, name):
+        self.run_gluster(['volume', 'start', name])
+
+    def stop_volume(self, name):
+        self.run_gluster(['volume', 'stop', name])
+
+    def delete_volume(self, name):
+        self.run_gluster(['volume', 'delete', name])
+
+    def create_volume(self, name: str, transport: str, hosts: list, bricks: list, force: bool):
+        args = ['volume', 'create']
+        args.append(name)
+        args.append('replica')
+        args.append(len(hosts) * len(bricks))
+        args.append('transport')
+        args.append(transport)
+        for brick in bricks:
+            for host in hosts:
+                args.append(('%s:%s' % (host, brick)))
+        if force:
+            args.append('force')
+        self.run_gluster(args)
+
+    def add_bricks(self, name: str, new_bricks: list, replica: int, force: bool):
+        args = ['volume', 'add-brick', name]
+        args.append('replica')
+        args.append(str(replica))
+        args.extend(new_bricks)
+        if force:
+            args.append('force')
+        self.run_gluster(args)
+
+    def remove_bricks(self, name: str, removed_bricks: list, replica: int):
+        # max-tries=12 with default_interval=10 secs
+        max_tries = 12
+        retries = 0
+        success = False
+        args = ['volume', 'remove-brick', name]
+        args.append('replica')
+        args.append(str(replica))
+        args.extend(removed_bricks)
+        args.append('force')
+        self.run_gluster(args)
