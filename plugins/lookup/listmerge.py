8e536e262b7f72b4cbec0448d2b2b0e9a9c3a102
ahmadali shafiee <mail@ahmadalli.net>
2021-01-23 12:17:37 +0330

    render template variables
    Signed-off-by: ahmadali shafiee <mail@ahmadalli.net>

--- plugins/lookup/listmerge.py
+++ plugins/lookup/listmerge.py
@@ -10,19 +10,23 @@ DOCUMENTATION = """
             required: True
         notes: []
 """
-from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.errors import AnsibleError, AnsibleUndefinedVariable
 from ansible.plugins.lookup import LookupBase
-from ansible.utils.display import Display
-import copy
-
-display = Display()
+from ansible.module_utils.six import string_types
 
 
 class LookupModule(LookupBase):
 
-    def run(self, keys, variables=None, **kwargs):
+    def run(self, terms, variables=None, **kwargs):
+        if variables is not None:
+            self._templar.available_variables = variables
+        myvars = getattr(self._templar, '_available_variables', {})
+
         var_names = []
-        for key in keys:
+        for term in terms:
+            if not isinstance(term, string_types):
+                raise AnsibleError('Invalid setting identifier, "{}" is not a string, its a {}'.format(
+                    term, type(term).__name__))
             var_names.extend(
                 [x for x in variables.keys() if x.startswith(key)])
 
@@ -31,12 +35,20 @@ class LookupModule(LookupBase):
 
         result = []
         for var_name in var_names:
-            var = variables[var_name]
+            try:
+                var = myvars[var_name]
+            except KeyError:
+                try:
+                    var = myvars['hostvars'][myvars['inventory_hostname']][var_name]
+                except KeyError:
+                    raise AnsibleUndefinedVariable(
+                        'No variable found with this name: {}}'.format(var_name))
             if var is None:
                 continue
 
             if isinstance(var, list):
-                result.extend(var)
+                for value in var:
+                    result.append(self._templar.template(value, fail_on_undefined=True))
             else:
                 raise AnsibleError(
                     'variables should be of type list. {} is of type {}'.format(var_names[0]))
